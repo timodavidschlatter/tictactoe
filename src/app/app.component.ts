3651f748961c42a941cbc4ab88afc27da216406b
Timo Schlatter <timo.schlatter@hotmail.com>
2024-08-21 13:39:14 +0200

    Working tic tac toe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TictactoeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/tictactoe/tictactoe.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+
+@Component({
+  selector: 'app-tictactoe',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tictactoe.component.html',
+  styleUrl: './tictactoe.component.css',
+})
+export class TictactoeComponent {
+  board: string[] = [];
+  currentPlayer: string = 'X';
+  winner: string | null = null; // Winner property can either be a string or null. Default init is null.
+
+  constructor() {
+    this.resetGame(); // Initialize the board when the component is created
+  }
+
+  makeMove(index: number): void {
+    if (!this.board[index] && !this.winner) {
+      this.board[index] = this.currentPlayer;
+      if (this.checkWinner()) {
+        this.winner = this.currentPlayer;
+      } else {
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      }
+    }
+  }
+
+  checkWinner(): boolean {
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return winPatterns.some((pattern) =>
+      pattern.every((index) => this.board[index] === this.currentPlayer)
+    );
+  }
+
+  resetGame(): void {
+    this.board = Array(9).fill(null); // Initialize the board with 9 null values
+    this.currentPlayer = 'X';
+    this.winner = null;
+  }
+}
